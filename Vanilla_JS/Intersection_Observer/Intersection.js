9bb474a311623f11fd0536ae72408928daff5bca
eotkd4791 <eotkd4791@gmail.com>
2020-05-14 03:15:56 +0900

    IntersectionObserver API

---
+++ Vanilla_JS/Intersection_Observer/Intersection.js
@@ -0,0 +1,28 @@
+const options = {
+  root: null,
+  rootMargin: "-250px 0px",
+  threshold: 0.05
+};
+
+const observer = new IntersectionObserver(callback, options); //콜백, 옵션
+
+document.querySelectorAll('.container p').forEach(p => {
+  observer.observe(p);
+  // console.log('watching', p.textContent);
+});
+
+function callback(entries, ob) {
+  console.log(ob);
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      console.log('intersecting');
+      // console.log(entry.target);
+      // console.log(entry.time, entry.intersectionRatio);
+
+      entry.target.classList.add('active');
+      // ob.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove('active');
+    }
+  })
+}
